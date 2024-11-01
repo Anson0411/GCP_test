61587de8d01ca9f1128ea5c0e5501a8df9be14ff
Anson0411 <151916732+Anson0411@users.noreply.github.com>
2024-11-01 16:24:35 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def index():
-    return 'hello'
-
 @app.route('/predict', method=['POST'])
 def postInput():
         # 取得前端傳過來的數值
